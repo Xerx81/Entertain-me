c67bd8633d5cf1b0711cc8241c14804f09dc945e
Yuvraj <ryuvraj832@gmail.com>
2024-08-07 17:07:14 +0530

    Program is able to scrape top 5 movies from the top 250 movies page.

--- entertain-me.py
+++ entertain-me.py
@@ -1,5 +1,24 @@
-import urllib3
+import requests
 from bs4 import BeautifulSoup
 
-http = urllib3.PoolManager()
+def scrape_imdb_top_5():
+  url = "https://www.imdb.com/chart/top/"
+  headers = {"User-Agent": "Mozilla/5.0"}
+  response = requests.get(url, headers=headers)
+  soup = BeautifulSoup(response.content, "lxml")
+  htmlTitle = soup.findAll(class_="ipc-title__text")
+  
+  i = 0
+  for title in htmlTitle:
+    if i == 5:
+      break
+    title = title.string
+    if "IMDb" in title or "Recently viewed" in title:
+      pass
+    else:
+      print(title.string)
+      i += 1
+    
 
+if __name__ == "__main__":
+  scrape_imdb_top_5()
