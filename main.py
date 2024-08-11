e2c2235ee527c4437d09f5818244031d45abf645
Yuvraj <ryuvraj832@gmail.com>
2024-08-11 13:30:35 +0530

    Improved the code structure

--- main.py
+++ main.py
@@ -9,13 +9,13 @@ if __name__ == "__main__":
   intents.message_content = True
   client = discord.Client(intents=intents)
 
-  urls = {
-    "top_movies": "https://www.imdb.com/chart/top/",
-    "title": "https://www.imdb.com/search/title/?title=",
-    "release": "https://www.imdb.com/search/title/?release_date=",
-    "movies": "https://www.imdb.com/search/title/?title_type=feature",
-    "series": "https://www.imdb.com/search/title/?title_type=tv_series",
-    "games": "https://www.imdb.com/search/title/?title_type=video_game",
+  search_commands = {
+    ".topmovies": "https://www.imdb.com/chart/top/",
+    ".movies": "https://www.imdb.com/search/title/?title_type=feature",
+    ".series": "https://www.imdb.com/search/title/?title_type=tv_series",
+    ".games": "https://www.imdb.com/search/title/?title_type=video_game",
+    ".title": "https://www.imdb.com/search/title/?title=",
+    ".release": "https://www.imdb.com/search/title/?release_date=",
   }
 
   @client.event
@@ -28,59 +28,42 @@ if __name__ == "__main__":
     if message.author == client.user:
       return
 
-    if message.content.startswith('.topmovies'):
-      url = urls['top_movies']
-      scraper = Scraper()
-      movie_msg = scraper.get_top_movies(url)
-      await message.channel.send(f"```{movie_msg}```")
+    if message.content.startswith('.'):
+      command = message.content.split()[0].lower()
 
-    if message.content.startswith('.movies'):
-      url = urls['movies']
-      scraper = Scraper()
-      movie_msg = scraper.advanced_search(url)
-      await message.channel.send(f"```{movie_msg}```")
+      if command in search_commands:
+        url = search_commands[command]
+        scraper = Scraper()
 
-    if message.content.startswith('.series'):
-      url = urls['series']
-      scraper = Scraper()
-      movie_msg = scraper.advanced_search(url)
-      await message.channel.send(f"```{movie_msg}```")
+        if command == '.topmovies':
+          movie_msg = scraper.get_top_movies(url)
+          await message.channel.send(f"```{movie_msg}```")
 
-    if message.content.startswith('.games'):
-      url = urls['games']
-      scraper = Scraper()
-      movie_msg = scraper.advanced_search(url)
-      await message.channel.send(f"```{movie_msg}```")    
+        elif command == '.title' or command == '.release':
+          input = ' '.join(message.content.split()[1:])
+          full_url = f"{url}{input}"
 
-    if message.content.startswith('.title'):
-      url = urls['title']
-      title = ' '.join(message.content.split()[1:])
-      full_url = f"{url}{title}"
+          movie_msg = scraper.advanced_search(full_url)
+          await message.channel.send(f"```{movie_msg}```")
+  
+        else:
+          movie_msg = scraper.advanced_search(url)
+          await message.channel.send(f"```{movie_msg}```")
 
-      scraper = Scraper()
-      movie_msg = scraper.advanced_search(full_url)
-      await message.channel.send(f"```{movie_msg}```")
+      elif command == '.help':
+        commands_info = [
+          ".topmovies - Gives list of top 5 movies of all time",
+          ".movies - Gives list of top 5 currently popular movies",
+          ".series - Gives list of top 5 currently popular series",
+          ".games - Gives list of top 5 currently popular games",
+          ".title item_name(e.g. star wars) - Gives list of top 5 items regarding the title",
+          ".release item_year(e.g. 2024) - Gives list of top 5 items from given year"
+          ]
+        msg = f"Commands:-\n\n\n{'\n\n'.join(commands_info)}"
+        await message.channel.send(f"```{msg}```")
 
-    if message.content.startswith('.release'):
-      url = urls['release']
-      release_year = ''.join(message.content.split()[1:])
-      full_url = f"{url}{release_year}"
-
-      scraper = Scraper()
-      movie_msg = scraper.advanced_search(full_url)
-      await message.channel.send(f"```{movie_msg}```")
-
-    if message.content.startswith('.help'):
-      commands_info = [
-        ".topmovies - Gives list of top 5 movies of all time",
-        ".movies - Gives list of top 5 currently popular movies",
-        ".series - Gives list of top 5 currently popular series",
-        ".games - Gives list of top 5 currently popular games",
-        ".title item_name(e.g. star wars) - Gives list of top 5 items regarding the title",
-        ".release item_year(e.g. 2024) - Gives list of top 5 items from given year"
-        ]
-      msg = f"Commands:-\n\n\n{'\n\n'.join(commands_info)}"
-      await message.channel.send(f"```{msg}```")
+      else:
+        await message.channel.send(f"```Invalid Command! Use '.help' for a list of commands.```")
 
   client.run(os.getenv('TOKEN'))
 
